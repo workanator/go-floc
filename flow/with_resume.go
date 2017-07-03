db08d200895b10e3b2a0f01e24ea84e5b9befe69
workanator <workanator@gmail.com>
2017-07-03 23:41:55 +0500

    Add Resume guard

---
+++ flow/with_resume.go
@@ -0,0 +1,57 @@
+package flow
+
+import floc "github.com/workanator/go-floc"
+
+type resumeableFlow struct {
+	parent floc.Flow
+}
+
+// ResumeFunc when invoked resumes the execution of the flow. Effective in
+// case the flow was Canceled ot Completed.
+type ResumeFunc func() floc.Flow
+
+// WithResume creates a new instance of the flow containing the parent flow
+// and a resume function which allows to resume execution of the flow.
+func WithResume(parent floc.Flow) (floc.Flow, ResumeFunc) {
+	flow := &resumeableFlow{
+		parent: parent,
+	}
+
+	resume := func() floc.Flow {
+		return parent
+	}
+
+	return flow, resume
+}
+
+// Done returns a channel that's closed when the flow done.
+// Successive calls to Done return the same value.
+func (f *resumeableFlow) Done() <-chan struct{} {
+	return f.parent.Done()
+}
+
+// Close finishes the flow and releases all underlying resources.
+func (f *resumeableFlow) Close() {
+	f.parent.Cancel(nil)
+}
+
+// Complete finishes the flow with success status and stops
+// execution of futher jobs if any.
+func (f *resumeableFlow) Complete(data interface{}) {
+	f.parent.Complete(data)
+}
+
+// Cancel cancels the execution of the flow.
+func (f *resumeableFlow) Cancel(data interface{}) {
+	f.parent.Cancel(data)
+}
+
+// Tests if the execution of the flow is either completed or canceled.
+func (f *resumeableFlow) IsFinished() bool {
+	return f.parent.IsFinished()
+}
+
+// Returns the result code and the result data of the flow.
+func (f *resumeableFlow) Result() (result floc.Result, data interface{}) {
+	return f.parent.Result()
+}
---
+++ guard/resume.go
@@ -0,0 +1,55 @@
+package guard
+
+import (
+	"fmt"
+
+	floc "github.com/workanator/go-floc"
+	"github.com/workanator/go-floc/flow"
+)
+
+// Resume resumes execution of the flow possibly canceled or completed by
+// the job. If filter is empty or nil execution will be resumed regardless
+// the reason it was finished. Otherwise execution will be resumed if the
+// reason it finished is in the filter result set.
+func Resume(filter floc.ResultSet, job floc.Job) floc.Job {
+	if len(filter) == 0 {
+		// Result filtering is omitted so make the job simple with resuming always
+		// happen.
+		return func(theFlow floc.Flow, theState floc.State, theUpdate floc.Update) {
+			resFlow, resume := flow.WithResume(theFlow)
+			defer resume()
+
+			job(resFlow, theState, theUpdate)
+		}
+	}
+
+	// Make the job which is aware of the result.
+	return func(theFlow floc.Flow, theState floc.State, theUpdate floc.Update) {
+		resFlow, resume := flow.WithResume(theFlow)
+		defer func() {
+			// Test if execution finished first
+			if resFlow.IsFinished() {
+				res, data := resFlow.Result()
+				if filter.Contains(res) {
+					// Resume execution
+					resume()
+				} else {
+					// Propagate the result
+					switch res {
+					case floc.Canceled:
+						theFlow.Cancel(data)
+
+					case floc.Completed:
+						theFlow.Complete(data)
+
+					default:
+						// Something is wrong
+						panic(fmt.Errorf("flow finished with unknown result %v and data %v", res, data))
+					}
+				}
+			}
+		}()
+
+		job(resFlow, theState, theUpdate)
+	}
+}
