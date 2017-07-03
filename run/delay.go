9135c67dd3689af004b066068d4e7e71d664632b
workanator <workanator@gmail.com>
2017-07-03 14:41:59 +0500

    Use time.Timer for delaying jobs

--- run/delay.go
+++ run/delay.go
@@ -9,9 +9,9 @@ import (
 // Delay runs jobs sequentially with delay.
 func Delay(delay time.Duration, jobs ...floc.Job) floc.Job {
 	return func(flow floc.Flow, state floc.State, update floc.Update) {
-		// Create a channel which help to catch the moment when delay exceeded
-		done := make(chan int)
-		defer close(done)
+		// Create timer
+		timer := time.NewTimer(delay)
+		defer timer.Stop()
 
 		for _, job := range jobs {
 			// Do not start the next job if the execution is finished
@@ -19,19 +19,19 @@ func Delay(delay time.Duration, jobs ...floc.Job) floc.Job {
 				return
 			}
 
-			// Run a job which will sleep for the given amount of time
-			go func() {
-				defer func() { done <- 0 }()
-				time.Sleep(delay)
-			}()
+			// Reset the timer
+			if !timer.Stop() {
+				<-timer.C
+			}
+			timer.Reset(delay)
 
-			// Wait until delay passed of the execution of the flow is finished
+			// Wait until delay passed or the execution of the flow is finished
 			select {
 			case <-flow.Done():
 				// The execution is finished
 				return
 
-			case <-done:
+			case <-timer.C:
 				// Do the job
 				job(flow, state, update)
 			}
