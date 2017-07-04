21bdc8572f9362ce7cf240423e115b9e07ed3954
workanator <workanator@gmail.com>
2017-07-04 11:51:10 +0500

    Make resumeable flow the Go way

--- flow/with_resume.go
+++ flow/with_resume.go
@@ -3,8 +3,8 @@ package flow
 import floc "github.com/workanator/go-floc"
 
 type resumeableFlow struct {
+	floc.Flow
 	parent floc.Flow
-	fake   floc.Flow
 }
 
 // ResumeFunc when invoked resumes the execution of the flow. Effective in
@@ -15,8 +15,8 @@ type ResumeFunc func() floc.Flow
 // and a resume function which allows to resume execution of the flow.
 func WithResume(parent floc.Flow) (floc.Flow, ResumeFunc) {
 	flow := &resumeableFlow{
+		Flow:   New(),
 		parent: parent,
-		fake:   New(),
 	}
 
 	resume := func() floc.Flow {
@@ -24,36 +24,4 @@ func WithResume(parent floc.Flow) (floc.Flow, ResumeFunc) {
 	}
 
 	return flow, resume
-}
-
-// Done returns a channel that's closed when the flow done.
-// Successive calls to Done return the same value.
-func (f *resumeableFlow) Done() <-chan struct{} {
-	return f.fake.Done()
-}
-
-// Close finishes the flow and releases all underlying resources.
-func (f *resumeableFlow) Close() {
-	f.fake.Cancel(nil)
-}
-
-// Complete finishes the flow with success status and stops
-// execution of futher jobs if any.
-func (f *resumeableFlow) Complete(data interface{}) {
-	f.fake.Complete(data)
-}
-
-// Cancel cancels the execution of the flow.
-func (f *resumeableFlow) Cancel(data interface{}) {
-	f.fake.Cancel(data)
-}
-
-// Tests if the execution of the flow is either completed or canceled.
-func (f *resumeableFlow) IsFinished() bool {
-	return f.fake.IsFinished()
-}
-
-// Returns the result code and the result data of the flow.
-func (f *resumeableFlow) Result() (result floc.Result, data interface{}) {
-	return f.fake.Result()
 }
