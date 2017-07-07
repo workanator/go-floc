077b9b998ffa4de07190559ed758581e4d39b36d
workanator <workanator@gmail.com>
2017-07-08 00:44:59 +0500

    Add Cancel job

---
+++ guard/cancel.go
@@ -0,0 +1,10 @@
+package guard
+
+import floc "github.com/workanator/go-floc"
+
+// Cancel cancels execution of the flow with the data given.
+func Cancel(data interface{}) floc.Job {
+	return func(flow floc.Flow, state floc.State, update floc.Update) {
+		flow.Cancel(data)
+	}
+}
