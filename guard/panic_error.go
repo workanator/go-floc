b157895eebfce0b1b89c6954b744b04b18da548d
workanator <workanator@gmail.com>
2017-07-18 00:44:11 +0500

    Rename struct fields and improve error message generation

--- guard/panic_error.go
+++ guard/panic_error.go
@@ -2,11 +2,22 @@ package guard
 
 import "fmt"
 
+const errorPrefix = "paniced with "
+
 // ErrPanic is thrown with Cancel if no panic trigger is provided to Panic.
 type ErrPanic struct {
-	err interface{}
+	Data interface{}
 }
 
 func (err ErrPanic) Error() string {
-	return fmt.Sprintf("%v", err.err)
+	switch v := err.Data.(type) {
+	case error:
+		return fmt.Sprintf("%s%s", errorPrefix, v.Error())
+
+	case fmt.Stringer:
+		return fmt.Sprintf("%s%s", errorPrefix, v.String())
+
+	default:
+		return fmt.Sprintf("%s%v", errorPrefix, err.Data)
+	}
 }
--- guard/timeout_error.go
+++ guard/timeout_error.go
@@ -7,10 +7,10 @@ import (
 
 // ErrTimeout is thrown with Cancel if no panic trigger is provided to Timeout.
 type ErrTimeout struct {
-	id interface{}
-	at time.Time
+	ID interface{}
+	At time.Time
 }
 
 func (err ErrTimeout) Error() string {
-	return fmt.Sprintf("%v timed out at %s", err.id, err.at)
+	return fmt.Sprintf("%v timed out at %s", err.ID, err.At)
 }
