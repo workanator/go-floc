d9327c59fe48048b60b16b9af059f4387b591c63
workanator <workanator@gmail.com>
2017-07-28 23:36:49 +0500

    Update docs

--- state.go
+++ state.go
@@ -2,8 +2,25 @@ package floc
 
 import "sync"
 
-// State is the container of data shared amongst jobs. Depending on
-// implementation the data can be thread-safe or not.
+/*
+State is the container of data shared amongst jobs. Depending on
+implementation the data can be thread-safe or not.
+
+The state is aware of possible implementation of Releaser interface by contained
+data. So if the contained data implements Releaser call to state.Release() will
+be propagated to data.Release() as well.
+
+  type Data struct{}
+
+  func (Data) Release() {
+    fmt.Println("Data released")
+  }
+
+  state := floc.NewState(Data{})
+  state.Release()
+
+  // Output: Data released
+*/
 type State interface {
 	Releaser
 
