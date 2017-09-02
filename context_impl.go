55b05d5461d8404a45bf646665a4d757643731b9
workanator <workanator@gmail.com>
2017-09-02 23:45:35 +0500

    Store Context in atomic Value

--- context_impl.go
+++ context_impl.go
@@ -3,18 +3,23 @@ package floc
 import (
 	"context"
 	"sync"
+	"sync/atomic"
 )
 
 type flowContext struct {
-	context.Context
-	sync.RWMutex
+	ctx atomic.Value
+	mu  sync.Mutex
 }
 
 func NewContext() Context {
-	return &flowContext{
-		Context: context.TODO(),
-		RWMutex: sync.RWMutex{},
+	ctx := &flowContext{
+		ctx: atomic.Value{},
+		mu:  sync.Mutex{},
 	}
+
+	ctx.ctx.Store(context.TODO())
+
+	return ctx
 }
 
 // Release releases resources.
@@ -24,34 +29,30 @@ func (flowCtx flowContext) Release() {
 
 // Ctx returns the underlying context.
 func (flowCtx flowContext) Ctx() context.Context {
-	flowCtx.RLock()
-	defer flowCtx.RUnlock()
-
-	return flowCtx.Context
+	return flowCtx.ctx.Load().(context.Context)
 }
 
 // UpdateCtx sets the new underlying context.
 func (flowCtx flowContext) UpdateCtx(ctx context.Context) {
-	flowCtx.Lock()
-	defer flowCtx.Unlock()
+	flowCtx.mu.Lock()
+	defer flowCtx.mu.Unlock()
 
-	flowCtx.Context = ctx
+	flowCtx.ctx.Store(ctx)
 }
 
 // Value returns the value associated with this context for key,
 // or nil if no value is associated with key.
 func (flowCtx flowContext) Value(key interface{}) (value interface{}) {
-	flowCtx.RLock()
-	defer flowCtx.RUnlock()
-
-	return flowCtx.Context.Value(key)
+	ctx := flowCtx.ctx.Load().(context.Context)
+	return ctx.Value(key)
 }
 
 // Create a new context with value and make it the current.
 func (flowCtx flowContext) AddValue(key, value interface{}) {
-	flowCtx.Lock()
-	defer flowCtx.Unlock()
+	flowCtx.mu.Lock()
+	defer flowCtx.mu.Unlock()
 
-	newCtx := context.WithValue(flowCtx.Context, key, value)
-	flowCtx.Context = newCtx
+	oldCtx := flowCtx.ctx.Load().(context.Context)
+	newCtx := context.WithValue(oldCtx, key, value)
+	flowCtx.ctx.Store(newCtx)
 }
