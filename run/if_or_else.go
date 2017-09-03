d0b13b9d5defe7db19d5a407f7247b9d69f5f830
workanator <workanator@gmail.com>
2017-09-04 00:35:58 +0500

    Add IfOrElse

---
+++ run/if_or_else.go
@@ -0,0 +1,38 @@
+package run
+
+import (
+	"github.com/workanator/go-floc.v2"
+)
+
+/*
+IfOrElse runs jobTrue if the condition is met or runs jobFalse otherwise.
+
+Summary:
+	- Run jobs in goroutines : NO
+	- Wait all jobs finish   : YES
+	- Run order              : SEQUENCE
+
+Diagram:
+                      +----->[JOB_TRUE]---+
+                      | YES               |
+  --(CONDITION MET?)--+                   +-->
+                      | NO                |
+                      +----->[JOB_FALSE]--+
+*/
+func IfOrElse(predicate floc.Predicate, jobTrue, jobFalse floc.Job) floc.Job {
+	return func(ctx floc.Context, ctrl floc.Control) error {
+		var err error
+
+		if predicate(ctx) {
+			err = jobTrue(ctx, ctrl)
+		} else {
+			err = jobFalse(ctx, ctrl)
+		}
+
+		if handleResult(ctrl, err) {
+			return err
+		}
+
+		return nil
+	}
+}
