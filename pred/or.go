c82809743387473bfdf843529d81072ad5348421
workanator <workanator@gmail.com>
2017-09-04 22:35:17 +0500

    Add Or

---
+++ pred/or.go
@@ -0,0 +1,35 @@
+package pred
+
+import (
+	"github.com/workanator/go-floc.v2"
+)
+
+// Or returns a predicate which chains multiple predicates into a condition
+// with OR logic. The result predicate finishes calculation of
+// the condition as fast as the result is known.
+//
+// The result predicate tests the condition as follows.
+//   [PRED_1] OR ... OR [PRED_N]
+func Or(predicates ...floc.Predicate) floc.Predicate {
+	count := len(predicates)
+	if count > 2 {
+		// More than 2 predicates
+		return func(ctx floc.Context) bool {
+			for _, predicate := range predicates {
+				if predicate(ctx) {
+					return true
+				}
+			}
+
+			return false
+		}
+	} else if count == 2 {
+		// 2 predicates
+		return func(ctx floc.Context) bool {
+			return predicates[0](ctx) || predicates[1](ctx)
+		}
+	}
+
+	// Require at least 2 predicates
+	panic("Or requires at least 2 predicates")
+}
