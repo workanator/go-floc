72aba510e448ea6bc0a260d3dc9dd098f6374b16
workanator <workanator@gmail.com>
2017-09-04 22:18:55 +0500

    Add Unless

---
+++ run/unless.go
@@ -0,0 +1,35 @@
+package run
+
+import (
+	"github.com/workanator/go-floc.v2"
+)
+
+const locUnless = "Unless"
+
+/*
+Unless runs the job if the condition is not met.
+
+Summary:
+	- Run jobs in goroutines : NO
+	- Wait all jobs finish   : YES
+	- Run order              : SEQUENCE
+
+Diagram:
+                      +-------------+
+                      | YES         |
+  --(CONDITION MET?)--+             +-->
+                      | NO          |
+                      +---->[JOB]---+
+*/
+func Unless(predicate floc.Predicate, job floc.Job) floc.Job {
+	return func(ctx floc.Context, ctrl floc.Control) error {
+		if !predicate(ctx) {
+			err := job(ctx, ctrl)
+			if handledErr := handleResult(ctrl, err, locUnless); handledErr != nil {
+				return handledErr
+			}
+		}
+
+		return nil
+	}
+}
