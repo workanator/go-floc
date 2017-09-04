45fedc4805e8881dd7782a0c6abcfa51fa297fd5
workanator <workanator@gmail.com>
2017-09-04 22:51:43 +0500

    Add Panic

---
+++ guard/err_panic.go
@@ -0,0 +1,23 @@
+package guard
+
+import "fmt"
+
+const errorPrefix = "panic with "
+
+// ErrPanic is thrown with Cancel if no panic trigger is provided to Panic.
+type ErrPanic struct {
+	Data interface{}
+}
+
+func (err ErrPanic) Error() string {
+	switch v := err.Data.(type) {
+	case error:
+		return errorPrefix + v.Error()
+
+	case fmt.Stringer:
+		return errorPrefix + v.String()
+
+	default:
+		return fmt.Sprintf("%s%v", errorPrefix, err.Data)
+	}
+}
---
+++ guard/panic.go
@@ -0,0 +1,47 @@
+package guard
+
+import (
+	"github.com/workanator/go-floc.v2"
+	"unidata/lib.coflow/coflow/flow"
+)
+
+const locPanic = "Panic"
+
+// PanicTrigger is triggered when the goroutine state is recovered after
+// panic.
+type PanicTrigger func(ctx floc.Context, ctrl floc.Control, v interface{})
+
+// Panic protects the job from falling into panic. On panic the flow will
+// be canceled with the ErrPanic result. Guarding the job from falling into
+// panic is effective only if the job runs in the current goroutine.
+func Panic(job floc.Job) floc.Job {
+	return OnPanic(job, nil)
+}
+
+// IgnorePanic protects the job from falling into panic. On panic the panic
+// will be ignored. Guarding the job from falling into
+// panic is effective only if the job runs in the current goroutine.
+func IgnorePanic(job floc.Job) floc.Job {
+	return OnPanic(job, func(ctx floc.Context, ctrl floc.Control, v interface{}) {})
+}
+
+// OnPanic protects the job from falling into panic. In addition it
+// takes PanicTrigger func which is called in case of panic. Guarding the job
+// from falling into panic is effective only if the job runs in the current
+// goroutine.
+func OnPanic(job floc.Job, panicTrigger PanicTrigger) floc.Job {
+	return func(ctx floc.Context, ctrl floc.Control) error {
+		defer func() {
+			if r := recover(); r != nil {
+				if panicTrigger != nil {
+					panicTrigger(ctx, ctrl, r)
+				} else {
+					ctrl.Cancel(ErrPanic{Data: r})
+				}
+			}
+		}()
+
+		// Do the job
+		return job(ctx, ctrl)
+	}
+}
