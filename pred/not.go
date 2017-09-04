91b4cfbad16e403758db143945c2f645c71a1957
workanator <workanator@gmail.com>
2017-09-04 22:34:09 +0500

    Add Not

---
+++ pred/not.go
@@ -0,0 +1,15 @@
+package pred
+
+import (
+	"github.com/workanator/go-floc.v2"
+)
+
+// Not returns the negated value of the predicate.
+//
+// The result predicate tests the condition as follows.
+//   NOT [PRED]
+func Not(predicate floc.Predicate) floc.Predicate {
+	return func(ctx floc.Context) bool {
+		return !predicate(ctx)
+	}
+}
