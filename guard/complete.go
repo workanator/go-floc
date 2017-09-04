d8526cb853dd865889577565897753f8ec94fcc1
workanator <workanator@gmail.com>
2017-09-04 22:41:39 +0500

    Add Complete

---
+++ guard/complete.go
@@ -0,0 +1,15 @@
+package guard
+
+import (
+	"github.com/workanator/go-floc.v2"
+)
+
+/*
+Complete completes execution of the flow with the data given.
+*/
+func Complete(data interface{}) floc.Job {
+	return func(ctx floc.Context, ctrl floc.Control) error {
+		ctrl.Complete(data)
+		return nil
+	}
+}
