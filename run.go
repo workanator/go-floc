f09789ca83f2a7c0d3f95b2ac676ba6c1fbd6540
workanator <workanator@gmail.com>
2017-09-18 23:09:55 +0500

    Add RunWith

--- run.go
+++ run.go
@@ -4,18 +4,25 @@ import "gopkg.in/workanator/go-floc.v2/errors"
 
 const locRun = "Run"
 
+// Run creates a new Context and Control and runs the flow.
 func Run(job Job) (result Result, data interface{}, err error) {
-	// Return invalid job error if the job is nil
-	if job == nil {
-		return None, nil, errors.ErrInvalidJob{}
-	}
-
 	// Create context and control
 	ctx := NewContext()
 	defer ctx.Release()
 
 	ctrl := NewControl(ctx)
 	defer ctrl.Release()
+
+	// Run the flow
+	return RunWith(ctx, ctrl, job)
+}
+
+// RunWith runs the flow with the Context and Control given.
+func RunWith(ctx Context, ctrl Control, job Job) (result Result, data interface{}, err error) {
+	// Return invalid job error if the job is nil
+	if job == nil {
+		return None, nil, errors.ErrInvalidJob{}
+	}
 
 	// Run the flow and return the result
 	unhandledErr := job(ctx, ctrl)
